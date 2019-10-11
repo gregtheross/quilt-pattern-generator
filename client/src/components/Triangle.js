2b6d148c1fd348373a2cb178258528395947cf0c
gregtheross <46449514+gregtheross@users.noreply.github.com>
2019-10-10 22:39:22 -0400

    added todo idea for svg triangles

--- client/src/components/Triangle.js
+++ client/src/components/Triangle.js
@@ -13,3 +13,18 @@ class Triangle extends React.Component {
 }
 
 export default Triangle;
+
+/*
+todo: experiment with converting to SVG:
+https://css-tricks.com/clipping-masking-css/
+
+<img class="clip-svg" src="http://i.imgur.com/RECDV24.jpg" alt="Harry Potter">
+
+<svg width="0" height="0">
+  <defs>
+    <clipPath id="myClip">
+      <polygon points="0 0, 300 0, 150 200" />
+    </clipPath>
+  </defs>
+</svg>
+*/
