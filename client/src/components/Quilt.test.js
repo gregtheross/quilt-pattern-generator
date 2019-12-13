8d9a25ee39b5c64795f9a9776bf351121587f987
gregtheross <greg.ross@kingsmensoftware.com>
2019-12-13 16:13:59 -0500

    playing around with PropTypes and testing the shapes using enzyme

---
+++ client/src/components/Quilt.test.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Quilt from "./Quilt";
+import FabricBlock from "./FabricBlock";
+
+it("should pass", () => {
+  expect(true).toEqual(true);
+});
+
+function renderQuilt(args) {
+  const defaultProps = {
+    rowCount: 1,
+    colCount: 1,
+    shapeType: "equilateral triangle",
+    shapeWidth: 100,
+    shapeHeight: 100,
+    fabricList: [],
+    quiltBlocks: [],
+    onFabricBlockClick: jest.fn()
+  };
+
+  const props = { ...defaultProps, ...args };
+
+  return mount(<Quilt {...props} />);
+}
+
+it("100x100 equilateral triangle", () => {
+  const wrapper = renderQuilt();
+  //   console.log(wrapper.debug());
+  const expextedPolygon = (
+    <polygon points="50 0, 100 86.60254037844386, 0 86.60254037844386" />
+  );
+
+  expect(wrapper.contains(expextedPolygon)).toEqual(true);
+});
+
+"square",
+  it("80x100 isosceles triangle", () => {
+    const wrapper = renderQuilt({
+      shapeType: "isosceles triangle",
+      shapeHeight: 80
+    });
+    // console.log(wrapper.debug());
+    const expextedPolygon = <polygon points="50 0, 100 80, 0 80" />;
+
+    expect(wrapper.contains(expextedPolygon)).toEqual(true);
+  });
+
+it("100x100 square", () => {
+  const wrapper = renderQuilt({ shapeType: "square" });
+  //   console.log(wrapper.debug());
+  const expextedPolygon = <polygon points="0 0, 100 0, 100 100, 0 100" />;
+
+  expect(wrapper.contains(expextedPolygon)).toEqual(true);
+});
+
+it("100x100 hexagon", () => {
+  const wrapper = renderQuilt({ shapeType: "hexagon" });
+  //   console.log(wrapper.debug());
+  const expextedPolygon = (
+    <polygon points="0 43.30127018922193, 25 0, 75 0, 100 43.30127018922193 75 86.60254037844386, 25 86.60254037844386" />
+  );
+
+  expect(wrapper.contains(expextedPolygon)).toEqual(true);
+});
